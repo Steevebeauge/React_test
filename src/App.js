09978821ebc35a3d4caa0ca4dfbe1547f30771c7
Steve <steevebeauge@yahoo.fr>
2022-04-06 20:02:33 +0200

    test react 70%

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
+import CardComponent from "./components/card";
 
-function App() {
+  // new test
+const cardImages = [
+  {'src': '/img/angular.png', matched: false},
+  {'src': '/img/vue.png', matched: false},
+  {'src': '/img/javaScript.png', matched: false},
+  {'src': '/img/node.png', matched: false},
+  {'src': '/img/php.png', matched: false},
+  {'src': '/img/react.png', matched: false},
+];
+
+export default function App() {
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [ChoiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+// next 
+
+const shuffleCards = () => {
+  const shuffledCards = [...cardImages, ...cardImages]
+    .sort(() => Math.random() - 0.5)
+    .map((card) => ({ ...card, id: Math.random() }))
+  
+  setChoiceOne(null)
+  setChoiceTwo(null)
+  setCards(shuffledCards)
+  setTurns(0)
+}
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  useEffect(() => {
+    if(choiceOne && ChoiceTwo) {
+      setDisabled(true)
+      if(choiceOne.src === ChoiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src) {
+              return {...card, matched: true}
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        setTimeout(() => resetTurn(), 1000)
+      }
+    }
+
+  }, [choiceOne, ChoiceTwo])
+  
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1)
+    setDisabled(false)
+  }
+  useEffect(() => {
+    shuffleCards()
+  }, []) 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Memory Game</h1>
+      <button onClick={shuffleCards}>
+          Restart
+      </button>
+      <div className="card-grid">
+        {cards.map( card => (
+          <CardComponent 
+            key={card.id} 
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === ChoiceTwo || card.matched}
+            disabled={disabled}
+          />   
+        ))}
+      </div>
+      <p> Turns: {turns}</p>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/card.js
@@ -0,0 +1,26 @@
+import './card.css'
+export default function CardComponent( { card, handleChoice, flipped, disabled }) {
+
+    const handleClick = () => {
+        if(!disabled) {
+            handleChoice(card)
+        } 
+    }  
+    return (
+        <div className="card">
+            <div className={flipped ? 'flipped' : ''}>
+              <img
+                className="front"
+                src={card.src}
+                alt="card front"
+              />
+              <img 
+                className="back" 
+                src="/img/java.png" 
+                onClick={handleClick} 
+                alt="card back"
+              />
+            </div>
+        </div>
+    )
+}
